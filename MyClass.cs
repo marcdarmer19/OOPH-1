cb77e604f205d77a552d5705eeda549b1035835e
marcdarmer19 <marcdarmer19@hotmail.com>
2021-03-02 08:53:08 +0100

    Add project files.

---
+++ MyClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOPH_1
+{
+    // framework vs. platform
+    // - værktøj, build, run (framework - web/desktop/mobil frameworks)
+    // - det som frameworks køre på er en platform.
+    //
+
+
+
+
+    class MyClass
+    {
+
+    }
+}
